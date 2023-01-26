5f522fb4f0acc716efab5d0e97f6ebebbb9a09bf
AlexandraAlt3 <ponkratova12@yandex.ru>
2023-01-26 09:46:37 +0300

    feat: finish task Reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  var strN = String(n);
+  var revInt='';
+  for (var i = 0; i < strN.length; i++){
+    let pR = n/10;
+    var perN = (pR - Math.floor(pR)) * 10;
+    perN = Math.round(perN);
+    n=(n-perN) / 10;
+    revInt += perN;
+  }
+  return revInt;
 }
+/*(function reverse (n) {
+  var strN = String(n);
+  var revInt='';
+  for (var i = 0; i < strN.length; i++){
+    let pR = n/10;
+    console.log('pR ='+pR);
+    console.log('Math.floor(pR) ='+ Math.floor(pR));
+    console.log('result='+ (pR - Math.floor(pR)));
+    var perN = (pR - Math.floor(pR)) * 10;
+    perN = Math.round(perN);
+    console.log('perN ='+perN);
+    n=(n-perN) / 10;
+    console.log('n ='+n);
+    revInt += perN;
+    console.log('revInt ='+revInt);
+
+  }
+    console.log (revInt);
+  return revInt;
+})(123)*/
